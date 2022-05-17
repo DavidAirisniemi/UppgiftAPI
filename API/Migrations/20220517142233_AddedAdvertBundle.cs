0f6ee9de620f68bc62b1e075f52a589809817a5c
David Airisniemi <David.Airisniemi@hotmail.com>
2022-05-17 16:32:31 +0200

    Updated migration

---
+++ API/Migrations/20220517142233_AddedAdvertBundle.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace API.Migrations
+{
+    public partial class AddedAdvertBundle : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "AdvertBundleDTO",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    AdvertId = table.Column<int>(type: "int", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_AdvertBundleDTO", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_AdvertBundleDTO_Advert_AdvertId",
+                        column: x => x.AdvertId,
+                        principalTable: "Advert",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_AdvertBundleDTO_AdvertId",
+                table: "AdvertBundleDTO",
+                column: "AdvertId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "AdvertBundleDTO");
+        }
+    }
+}
